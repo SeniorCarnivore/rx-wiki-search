665f061911cffb9a89186be94a1863386994751b
Igor Alekseev <ialekseev@devexperts.com>
2019-04-02 17:23:47 +0300

    fix typings

--- src/app/app.container.ts
+++ src/app/app.container.ts
@@ -2,7 +2,7 @@ import { WikiResponse } from "./../api/WikiApi";
 import { fetchWikiArticles } from "../api/WikiApi";
 import { timer, combineLatest, BehaviorSubject } from "rxjs";
 import { filter, debounce, switchMap } from "rxjs/operators";
-import { withRX } from "@devexperts/react-kit/dist/utils/with-rx2";
+import { withRX, Observify } from "@devexperts/react-kit/dist/utils/with-rx2";
 import { pending } from "@devexperts/remote-data-ts";
 import { mapRD } from "@devexperts/rx-utils/dist/rd/operators/mapRD";
 // import { map, tap } from "rxjs/operators";
@@ -23,14 +23,14 @@ const results$ = combineLatest(queryForFetch$, limit$).pipe(
   switchMap(([query, limit]) => fetchWikiArticles(query, limit))
 );
 
-const defaultProps = {
+const defaultProps: Partial<AppProps> = {
   limits,
   results: pending,
   onQueryChange: (value: string) => query$.next(value),
   onLimitChange: (value: number) => limit$.next(value)
 };
 
-const props = {
+const props: Partial<Observify<AppProps>> = {
   results: results$.pipe(mapRD(wikiResp => toWikiData(wikiResp)))
 };
 
